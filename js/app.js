9ce4a888a09b5da07bebdfd3e0402f90215703d8
nesaranaeem <hello@nesaran.com>
2022-08-28 17:47:58 +0600

    Love & Happiness quotes added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const quotes = (keyword) => {
+const randomQuotes = (keyword) => {
   const url = `https://api.quotable.io/${keyword}`;
   fetch(url)
     .then((response) => response.json())
@@ -15,4 +15,35 @@ const randomQuote = (data) => {
   <cite title="${data.author}">${data.author}</cite>
   `;
 };
-quotes("random");
+
+//new
+const loadLoveQuotes = () => {
+  fetch("https://quotable.io/quotes?tags=love%7Chappiness")
+    .then((res) => res.json())
+    .then((data) => displayUsers(data.results));
+};
+
+const displayUsers = (quotes) => {
+  const loveHapiness = document.getElementById("love-happiness");
+  for (const quote of quotes) {
+    console.log(quote);
+    const loveHapinessQuotes = document.createElement("div");
+    loveHapinessQuotes.classList.add("col-lg-4");
+    loveHapinessQuotes.classList.add("col-md-6");
+    loveHapinessQuotes.innerHTML = `
+          
+          <!-- start -->
+         
+            <div class="border bg-light h-100">
+            <p>${quote.content}</p>
+            <cite title="${quote.author}">By- ${quote.author}</cite>
+            </div>
+         
+          <!-- end -->
+      `;
+    loveHapiness.appendChild(loveHapinessQuotes);
+  }
+};
+
+loadLoveQuotes();
+randomQuotes("random");
